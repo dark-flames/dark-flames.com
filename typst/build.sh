c5fa733d6f4bde1a768475179ae956ac680108a9
dark-flames <dark_flames@outlook.com>
2025-06-08 07:13:05 +0000

    fe: fix page scale

--- typst/build.sh
+++ typst/build.sh
@@ -3,18 +3,23 @@
 # Get all subdirectories in the current working directory
 subpaths=$(find . -mindepth 1 -maxdepth 1 -type d)
 
+<<<<<<< HEAD
 rm -rf ../static/typst/*
+=======
+mkdir -p ../static/typst
+>>>>>>> 9b81527 (fe: fix page scale)
 
 # Loop through each subpath
 for p in $subpaths; do
-  # Change directory to the subpath
-  # Execute the typst-ts-cli compile command
     dir_name=$(basename "$p")
     
-    mkdir ../static/typst/$dir_name
-    typst-ts-cli compile \
+    mkdir -p ../static/typst/$dir_name
+
+    echo "Compile workspace: $dir_name"
+      typst-ts-cli compile \
         --workspace "$p" \
         --entry "$p/main.typ"\
         --output "../static/typst/$dir_name" \
         --dynamic-layout
+    
 done
